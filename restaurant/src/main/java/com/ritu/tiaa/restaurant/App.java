e9ef8eed0c462006372b8742dacf2083d9a57b03
Ritu Raj <rraj@ptc.com>
2018-08-05 12:34:15 +0530

    Minor changes

--- restaurant/src/main/java/com/ritu/tiaa/restaurant/App.java
+++ restaurant/src/main/java/com/ritu/tiaa/restaurant/App.java
@@ -1,5 +1,9 @@
 package com.ritu.tiaa.restaurant;
 
+import com.ritu.tiaa.restaurant.Files;
+import com.ritu.tiaa.restaurant.JSONFiles;
+import com.ritu.tiaa.restaurant.XMLFiles;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Files xmlfile = new  XMLFiles();
+        xmlfile.readFile("src/main/java/com/ritu/tiaa/testdata/BOM-1234.xml");
+        Files jsonFiles = new  JSONFiles();
+        jsonFiles.readFile("src/main/java/com/ritu/tiaa/testdata/BOM-4567.json");
+        System.out.println("Success");
     }
 }
