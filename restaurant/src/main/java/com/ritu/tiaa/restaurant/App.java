edb7da752c6e2bbf18c02deba546b7d207fd351b
Ritu Raj <rraj@ptc.com>
2018-08-05 11:05:51 +0530

    first commit

---
+++ restaurant/src/main/java/com/ritu/tiaa/restaurant/App.java
@@ -0,0 +1,13 @@
+package com.ritu.tiaa.restaurant;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
